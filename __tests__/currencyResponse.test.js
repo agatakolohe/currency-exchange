3bc31d5077aee56b2855d53be10e59c297989998
agatakolohe <agatakolohe@gmail.com>
2020-11-20 10:22:21 -0800

    test return cad conversion rate, good fail

--- __tests__/currencyResponse.test.js
+++ __tests__/currencyResponse.test.js
@@ -13,4 +13,7 @@ describe('CurrencyResponse', () => {
   test('it should correctly return the conversion rates of CurrencyResponse', () => {
     expect(testCurrency.conversion_rates).toEqual({"CAD": 1.3089, "EUR": 0.844, "GBP": 0.7552, "JPY": 103.9219, "PLN": 3.7779, "USD": 1});
   });
+  test('it should correctly return the conversion rate of CAD', () => {
+    expect(testCurrency.conversion_rates.CAD).toEqual(1.308);
+  });
 });
