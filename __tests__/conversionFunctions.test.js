252b95c9ac5251ca1a8c0ded1ea191ef86bef583
agatakolohe <agatakolohe@gmail.com>
2020-11-20 10:37:23 -0800

    create spec and constructor for conversion calculator

---
+++ __tests__/conversionFunctions.test.js
@@ -0,0 +1,9 @@
+export default class ConversionCalculator {
+  constructor(usd, currency) {
+    this.usd = usd;
+    this.currency = currency;
+  }
+  showMeTheMoney() {
+    return this.usd * this.currency;
+  } 
+}
