55db05572cdd30286053ca793f6f2c6aafe05ba1
agatakolohe <agatakolohe@gmail.com>
2020-11-20 14:41:10 -0800

    change != country to undefined, all functions work

--- src/js/conversionFunctions.js
+++
@@ -1,9 +0,0 @@
-export default class ConversionCalculator {
-  constructor(usd, currency) {
-    this.usd = usd;
-    this.currency = currency;
-  }
-  showMeTheMoney() {
-    return (this.usd * this.currency).toFixed(2);
-  } 
-}
--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import CurrencyExchange from './currencyAPI.js'
+import CurrencyExchange from './currencyAPI.js';
 
 
 function clearFields() {
@@ -15,20 +15,14 @@ function getElements(response, inputtedDollar, inputtedCurrencyType) {
     for (const [country, amount] of conversionRates) {
       if (inputtedCurrencyType === country) {
         let usdConversion = (inputtedDollar * amount).toFixed(2);
-        $('#showMoney').text(`$ ${inputtedDollar} is ${usdConversion} in ${country}`); 
+        $('#response').text(`$ ${inputtedDollar} is ${usdConversion} in ${country}`); 
         break;
-      } 
-  }
-    const checkCountries = Map.keys(response.conversion_rates);
-    for(const country of checkCountries) {
-      if (inputtedCurrencyType !== country) {
-        $('.noCountry').text(`The currency for ${inputtedCurrencyType} does not exist`);
-        console.log(checkCountries);
-        break;
+      } else if (inputtedCurrencyType === undefined) {
+        $('.noCountry').text(`The currency for ${inputtedCurrencyType} does not exist. Please select a currency`);
       }
     }
   } else {
-      $('.showErrors').text(`There was an error: ${response}`);
+    $('.showErrors').text(`There was an error: ${response}`);
   }
 }
 
@@ -39,30 +33,12 @@ async function makeApiCall(inputtedDollar, inputtedCurrencyType){
 
 //User Logic
 
- $(document).ready(function() {
-$("#moneyExchange").submit(function(event) {
+$(document).ready(function() {
+  $("#moneyExchange").submit(function(event) {
     event.preventDefault();
     let inputtedDollar = parseInt($("input#dollarAmount").val());
     let inputtedCurrencyType = $("input:radio[name=currencyTypes]:checked").val();
     clearFields();
-    console.log(typeof(inputtedCurrencyType));
     makeApiCall(inputtedDollar, inputtedCurrencyType);
   });
 });
-
-// if (inputtedCurrencyType === "canada") {
-//   let convertCa= (inputtedDollar * response.conversion_rates.CAD).toFixed(2);
-//   $('#showMoney').text(`${convertCa}`);
-// } else if (inputtedCurrencyType === "euro") {
-//   let convertEu = (inputtedDollar * response.conversion_rates.EUR).toFixed(2);
-//   $('#showMoney').text(`${convertEu}`);
-// } else if (inputtedCurrencyType === "uk") {
-//   let convertUk = (inputtedDollar * response.conversion_rates.GBP).toFixed(2);
-//   $('#showMoney').text(`${convertUk}`);
-// } else if (inputtedCurrencyType === "japan") {
-//   let convertJp = (inputtedDollar * response.conversion_rates.JPY).toFixed(2);
-//   $('#showMoney').text(`${convertJp}`);
-// } else if (inputtedCurrencyType === "poland") {
-//   let convertPl = (inputtedDollar * response.conversion_rates.PLN).toFixed(2);
-//   $('#showMoney').text(`${convertPl}`);
-// }
