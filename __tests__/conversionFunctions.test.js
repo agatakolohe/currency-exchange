1f14d7b8d0bc37d7c723adacd3365296ff4acff6
agatakolohe <agatakolohe@gmail.com>
2020-11-20 10:42:36 -0800

    test converstioncalculator object good fail and pass

--- __tests__/conversionFunctions.test.js
+++ __tests__/conversionFunctions.test.js
@@ -1,9 +1,13 @@
-export default class ConversionCalculator {
-  constructor(usd, currency) {
-    this.usd = usd;
-    this.currency = currency;
-  }
-  showMeTheMoney() {
-    return this.usd * this.currency;
-  } 
-}
+import { TestScheduler } from 'jest'
+import ConversionCalculator from './../src/js/conversionFunctions'
+
+describe('ConversionCalulator', () => {
+  
+  test('should correctly create a ConversionCalculator object', () => {
+    const canadaDollar = new ConversionCalculator("usd", "cad");
+    expect(canadaDollar.usd).toEqual("usd");
+    expect(canadaDollar.currency).toEqual("cad");
+  });
+
+
+});
